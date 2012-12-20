68b58036255bb3e26e25ae77171b142dd5ec7150
Yutaka SAITO <pxn11432@nifty.com>
2012-12-20 10:26:30 +0900

    setup.bat

---
+++ guest/setup.bat
@@ -0,0 +1,15 @@
+@echo off
+set UNZIP="C:\Program Files\7-Zip\7z.exe"
+if not exist %UNZIP% set UNZIP="C:\Program Files (x86)\7-Zip\7z.exe"
+rem ---------------------------------------------------------------------------
+set VCVARSALL="C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+if not exist %VCVARSALL% set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
+call %VCVARSALL%
+rem ---------------------------------------------------------------------------
+%UNZIP% x -y wxMSW-2.8.12.zip
+%UNZIP% x -y wxMSW-2.8.12-gurapatch.zip
+msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
+rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=x64
+rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Debug /p:Platform=win32
+rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Debug /p:Platform=x64
+pause
