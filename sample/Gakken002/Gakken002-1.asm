f64cdbe20c1a2fbc549eae7c9195c435df41cd0d
Yutaka SAITO <pxn11432@nifty.com>
2012-12-20 11:37:23 +0900

    added sample

---
+++ sample/Gakken002/Gakken002-1.asm
@@ -0,0 +1,7 @@
+; [学研] FX-マイコン R-165 プログラム集
+; No.2 自動演奏 - 「さくら さくら」
+mml	t72
+mml	aab2 aab2 ab>c<b ab8a8f2
+mml	ecef ee8c8<b2 >ab>c<b ab8a8f2
+mml	ecef ee8c8<b2 >aab2 aab2 r1
+dn	f, 0	; 繰り返し
---
+++ sample/Gakken002/Gakken002-2.asm
@@ -0,0 +1,7 @@
+; [学研] FX-マイコン R-165 プログラム集
+; No.2 自動演奏 - 「証城寺の狸ばやし」
+mml	t88
+mml	ce g8e8c8r8 g8e8d8c8 d8e8g8r8
+mml	eg >c8e8d8c8 <g8.g16a8g8 >c8c8c8r8<
+mml	f8e8d8c8 d8e8g8g8 >c8<a16g16e8d8 cr
+dn	f, 0	; 繰り返し
