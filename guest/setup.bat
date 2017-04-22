0ef850f8cea9769dcee3dbae215cf0b30736d78d
ypsitau <ypsitau@nifty.com>
2017-04-22 16:33:08 +0900

    updated vs project

--- guest/setup.bat
+++ guest/setup.bat
@@ -4,10 +4,6 @@ rem ---------------------------------------------------------------------------
 set VCVERSION=14.0
 call "C:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
 rem ---------------------------------------------------------------------------
-%UNZIP% x -y wxMSW-2.8.12.zip
-%UNZIP% x -y wxMSW-2.8.12-gmc4sim-patch.zip
-msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
-rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=x64
-rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Debug /p:Platform=win32
-rem msbuild wxMSW-2.8.12\build\msw\wx.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Debug /p:Platform=x64
+%UNZIP% x -y -owxWidgets-3.1.0 wxWidgets-3.1.0.7z
+msbuild wxWidgets-3.1.0\build\msw\wx_vc14.sln /clp:DisableConsoleColor /t:Build /p:Configuration=Release /p:Platform=win32
 pause
