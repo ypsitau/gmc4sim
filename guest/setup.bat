8098a83a8060163636939f7116f048c265ac0d4c
ypsitau <ypsitau@nifty.com>
2017-04-22 13:01:27 +0900

    fixed setup batch file

--- guest/setup.bat
+++ guest/setup.bat
@@ -1,10 +1,8 @@
 @echo off
-set UNZIP="C:\Program Files\7-Zip\7z.exe"
-if not exist %UNZIP% set UNZIP="C:\Program Files (x86)\7-Zip\7z.exe"
+set UNZIP="7za920\7za.exe"
 rem ---------------------------------------------------------------------------
-set VCVARSALL="C:\Program Files\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-if not exist %VCVARSALL% set VCVARSALL="C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
-call %VCVARSALL%
+set VCVERSION=14.0
+call "C:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
 rem ---------------------------------------------------------------------------
 %UNZIP% x -y wxMSW-2.8.12.zip
 %UNZIP% x -y wxMSW-2.8.12-gmc4sim-patch.zip
