f4f3cdfeb099ea0442a933b6ecefd3686faae853
ypsitau <ypsitau@nifty.com>
2017-04-23 21:04:58 +0900

    created a batch to create a distribution package

---
+++ build-dist.bat
@@ -0,0 +1,9 @@
+@echo off
+set UNZIP="guest\7za920\7za.exe"
+del gmc4sim-1.39.zip
+rmdir /s gmc4sim
+mkdir gmc4sim
+copy bin-x86\*.exe gmc4sim
+copy bin-x86\*.dll gmc4sim
+xcopy /s /i sample gmc4sim\sample
+%UNZIP% a gmc4sim-1.39.zip gmc4sim
