da93014774e617b2df73f0034e91ce3116c72d9f
ypsitau <ypsitau@nifty.com>
2017-04-24 10:22:58 +0900

    added document in package

--- build-dist.bat
+++ build-dist.bat
@@ -3,8 +3,10 @@ set UNZIP="guest\7za920\7za.exe"
 del gmc4sim-1.39.zip
 rmdir /s /q gmc4sim
 mkdir gmc4sim
+mkdir gmc4sim\doc
 copy bin-x86\*.exe gmc4sim
 copy bin-x86\*.dll gmc4sim
+copy doc\*.pdf gmc4sim\doc
 xcopy /s /i sample gmc4sim\sample
 %UNZIP% a gmc4sim-1.39.zip gmc4sim
 rmdir /s /q gmc4sim
